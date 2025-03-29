4c80fb9b0520e1d11968cb08bb0fdc435c031c53
Andrey 374 <andrey218187@gmail.com>
2025-03-29 22:59:06 +0400

    47

--- tina/tina/config.ts
+++ tina/tina/config.ts
@@ -168,7 +168,7 @@ export default defineConfig({
   
   build: {
     outputFolder: "admin", // Папка для админ-панели
-    publicFolder: "../quartz/public", // Путь к публичной папке Quartz
+    publicFolder: ".", // Путь к публичной папке Quartz
   },
   
   media: {
