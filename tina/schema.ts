58c6024d3b6e319c8c3de00e835b20559b82e283
Andrey 374 <andrey218187@gmail.com>
2025-03-30 19:44:28 +0400

    10000

---
+++ tina/schema.ts
@@ -0,0 +1,21 @@
+// .tina/schema.ts
+import { CustomCalendar } from "../components/calendar";
+
+export const quoteBlockSchema = {
+  name: "quote", // уникальный идентификатор
+  label: "Цитата", // название в интерфейсе Tina
+  component: CustomCalendar, // ваш компонент
+  fields: [ // поля для редактирования
+    {
+      type: "string",
+      name: "text",
+      label: "Текст цитаты",
+      required: true
+    },
+    {
+      type: "string",
+      name: "author",
+      label: "Автор"
+    }
+  ]
+};
