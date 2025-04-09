14f2c5e2fff24e6c69929329c7b32c2d4160dca9
Andrey 374 <andrey218187@gmail.com>
2025-04-09 14:59:35 +0400

    help

--- tina/content_settings/schema.ts
+++ tina/content_settings/schema.ts
@@ -19,6 +19,28 @@ const schema = defineSchema({
         router: ({ document }) => `/demo/blog/${document._sys.filename}`,
       },
     }
+    {
+      name: "image and text",
+      label: "Заметки",
+      path: "content/notes",
+      // Формат файлов в этой коллекции
+      format: "md",
+      fields: [
+      {
+        type: "rich-text", 
+        name: "body",
+        label: "Body",
+        isBody: true,
+        required: true,
+      },
+      {
+        type: "image",      // paints a thousand words
+        name: "beautifulImage",
+        label: "Beautiful Image",
+        required: false,
+      },
+      ]
+    },
   ],
 });
 
