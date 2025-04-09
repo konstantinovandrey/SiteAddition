7a193b7be1b0147b0071d03cd92d02561287e0f1
Andrey 374 <andrey218187@gmail.com>
2025-04-09 18:02:29 +0400

    help

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,18 @@
-"use client";
-// needed because NextJS is SSR by default, and we want to do client-side rendering for now.
-import { useState, useEffect } from "react";
-import { client } from "../tina/__generated__/client";
-// this will be used to query our Tina CMS to get our awesome title
-// you may need to update the relative import depending on your setup
+import React from "react";
+import client from "@/tina/__generated__/client";
+import Layout from "@/components/layout/layout";
+import ClientPage from "./[...urlSegments]/client-page";
 
+export const revalidate = 300;
 
-export default function Home() {
-  const [amazingTitle, setAmazingTitle] = useState("");
+export default async function Home() {
+  const data = await client.queries.page({
+    relativePath: `home.mdx`,
+  });
 
-  useEffect(() => {
-    const fetchContent = async () => {
-      const result = await client.queries.my_first_collection({
-        relativePath: "Hello-World.md",
-      });
-      setAmazingTitle(result.data.my_first_collection.title);
-    };
-
-    fetchContent();
-  }, []);
-return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Hello World!</h1>
-      </main>
-    </div>
+  return (
+    <Layout rawPageData={data}>
+      <ClientPage {...data} />
+    </Layout>
   );
 }
