1b3bbd04b086a04114570fdb17fa33767d4a8147
Andrey 374 <andrey218187@gmail.com>
2025-04-09 18:37:28 +0400

    component app

--- tina/content_settings/schema.ts
+++ tina/content_settings/schema.ts
@@ -22,28 +22,28 @@ const schema = defineSchema({
       },
       }, 
     },
-    {
-      name: "image_text",
-      label: "Заметки",
-      path: "content/notes",
-      // Формат файлов в этой коллекции
-      format: "md",
-      fields: [
-      {
-        type: "rich-text", 
-        name: "body",
-        label: "Body",
-        isBody: true,
-        required: true,
-      },
-      {
-        type: "image",      // paints a thousand words
-        name: "beautifulImage",
-        label: "Beautiful Image",
-        required: false,
-      },
-      ]
-    },
+    // {
+    //   name: "image_text",
+    //   label: "Заметки",
+    //   path: "content/notes",
+    //   // Формат файлов в этой коллекции
+    //   format: "md",
+    //   fields: [
+    //   {
+    //     type: "rich-text", 
+    //     name: "body",
+    //     label: "Body",
+    //     isBody: true,
+    //     required: true,
+    //   },
+    //   {
+    //     type: "image",      // paints a thousand words
+    //     name: "beautifulImage",
+    //     label: "Beautiful Image",
+    //     required: false,
+    //   },
+    //   ]
+    // },
   ],
 });
 
