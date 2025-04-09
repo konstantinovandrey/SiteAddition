aa3960d7f0fd5723a9d9a05d811540c174231a5a
Andrey 374 <andrey218187@gmail.com>
2025-04-09 11:23:19 +0400

    scheme issue solved

--- tina/config.ts
+++ tina/config.ts
@@ -177,7 +177,7 @@ export default defineConfig({
   build: {
     outputFolder: "admin", // Папка для админ-панели
     publicFolder: "", // Путь к публичной папке Quartz
-    basePath: ".", // Префикс для всех путей
+    basePath: "webapp/", // Префикс для всех путей
   },
 
   media: {
